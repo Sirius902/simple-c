60e78f500ce6ef66e0eec1b35faabf7ee994e44b
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-05-01 17:48:10 -0700

    Update main printing

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use simple_c::lexer::TokenStream;
 pub fn main() {
     let stream = TokenStream::new(r"49r14.63 chicken if (6}");
 
-    for lexeme in stream.into_iter() {
-        println!("{:?}", lexeme);
+    for res in stream.into_iter() {
+        match res {
+            Ok(lexeme) => println!("{:?}", lexeme),
+            Err(e) => println!("{}", e),
+        }
     }
 }
